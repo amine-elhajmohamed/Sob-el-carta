1e634044f46dd3aebb2842af921a470a6c6f89cd
MedAmine <mahm.ze@gmail.com>
2018-08-26 00:24:32 +0100

    Initial Commit

---
+++ Sob el carta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sob el carta
+//
+//  Created by MedAmine on 8/26/18.
+//  Copyright © 2018 AppGeek+. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
